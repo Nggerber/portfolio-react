55981fefc8af2cbbdb2440a5f64bb3730e01ebf3
nggerber <gerberclimbs@gmail.com>
2021-01-07 11:11:21 -0700

    added content to about me page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import Container from "react-bootstrap/Container"
+import Image from "react-bootstrap/Image"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import profilepic from "../../Images/profilepicture.png"
 
 function About(props) {
     return (
-        <h1>About</h1>  
+        <div>
+
+            <Container fluid>
+                <Row>
+                    <Col><h2 className="font-weight-bolder p-5">About Me</h2></Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <Image className="block-example border border-dark rounded mb-0" src={profilepic} fluid alt="me with my millenium falcon shirt" />
+                    </Col>
+                    <Col sm={5}>
+                        <h6 className="block-example border border-dark rounded mb-0 p-1">Hello my name is Neal Gerber, I was born and raised in Boulder Colorado.
+                        Growing up so close to the mountains had a profound impact on me. Be it
+                        backpacking in the Rocky Mountains, trail running around town or rock
+                        climbing I felt at home outdoors. This respect for the outdoors took me to Eugene
+                        Oregon for my undergraduate degree of biology but it really served to fuel
+                        my passion for rock climbing. After school I moved to Salt Lake City to be closer
+                        the mountains and see how far I could push my climbing. After six years of working
+                        in the outdoor industry I sought a career change that would support my lifestyle
+                        and encourage my creativity. For this I turned to my I turned to my other passion
+                        is suprisingly computers. I've always enjoyed messing around on a computer, playing
+                        video games or exploring the far reaches of the interenet. During highschool I had
+                        taken several coding courses in Java and C++ which I thouroghly enjoyed so much so that
+                        I took a couple more in college. Unfortunately I never viewed computer science as
+                        a career path until now. This has led me to the fullstack program where I hope to
+                        lay the foundation for a new career in web development. I think that I will be able to apply
+                        my hardworking personality to the world of web development and really be an excellent addition to any
+                    team.</h6>
+                    </Col>
+                </Row>
+            </Container>
+
+
+
+        </div>
+
     )
 }
 
--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import AvatarImg from "./Avatar"
+import Container from 'react-bootstrap/Container'
+
+
+
 
 function HomePage(props) {
     return (
-        <h1>HomePage</h1>  
+        <div>
+            <h1 className="p-0 text-center font-weight-bolder">A Passion for Learning</h1>
+            <Container className="avatar-div text-center" style={{width: "100%"}}>
+                <AvatarImg />
+            </Container>
+            <h4 className="p-0 text-center p-5">HTML JavaScript CSS REACT MySQL MongoDB Socket.io</h4>
+        </div>
     )
 }
 
