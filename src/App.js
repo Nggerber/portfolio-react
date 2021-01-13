a8d10854660b19f09bbd92fc26cabd53455c1f6a
nggerber <gerberclimbs@gmail.com>
2021-01-12 18:07:48 -0700

    fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import Container from "react-bootstrap/Container"
 import './App.css';
 
